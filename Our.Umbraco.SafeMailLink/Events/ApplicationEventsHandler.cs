d3c7ed442fbe68e9adacd342a34aa97b64ab91ca
vertino <none@none>
2012-10-08 15:13:34 +0100

    Update MSBuild to auto-generate version numbers. Moved 'PreApplicationStartMethod' outside of AssemblyInfo.

--- Our.Umbraco.SafeMailLink/Events/ApplicationEventsHandler.cs
+++ Our.Umbraco.SafeMailLink/Events/ApplicationEventsHandler.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+﻿using System.Web;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using Our.Umbraco.SafeMailLink.Events;
 using Our.Umbraco.SafeMailLink.Modules;
 using umbraco.BusinessLogic;
+
+[assembly: PreApplicationStartMethod(typeof(ApplicationEventsHandler), "RegisterModules")]
 
 namespace Our.Umbraco.SafeMailLink.Events
 {
