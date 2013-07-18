9f67cd8ec5ce91b6b155de2ac7f025528f088f77
leekelleher <leekelleher@gmail.com>
2013-07-18 10:19:19 +0100

    Modified the package build script.
    Bumped version number to v2.0.0 (breaking changes, due to code removal)

--- build.cmd
+++ build.cmd
@@ -1,2 +1,6 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe BuildPackage.msbuild
-PAUSE
+%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe package\package.proj
+@IF %ERRORLEVEL% NEQ 0 GOTO err
+@exit /B 0
+:err
+@PAUSE
+@exit /B 1
